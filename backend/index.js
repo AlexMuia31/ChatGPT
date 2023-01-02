7c5ff211e03a06cd0cdec2b47ca57ae69273f43f
AlexMuia31 <alexmuia31@gmail.com>
2023-01-02 12:01:13 +0300

    Cors and body parser

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
+//Body parser and CORS
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const configuration = new Configuration({
   organization: "org-p5wzQcxQJ8SKgmz5rkK9zPCB",
   apiKey: process.env.OPENAI_API_KEY,
@@ -10,17 +14,22 @@ const openai = new OpenAIApi(configuration);
 //const response = await openai.listEngines();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 const port = 3080;
 
 app.post("/", async (req, res) => {
+  const { message } = req.body;
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
   });
-  console.log(response.data.choices[0].text);
-  res.json({ data: response.data });
+
+  res.json({
+    message: response.data.choices[0].text,
+  });
 });
 
 app.listen(port, () => {
